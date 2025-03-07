bdbba8ac96e7e9d22cef9b069f62e6054a625e44
Lucas Cerqueira <lukascerqueira@live.com>
2025-03-06 22:08:21 -0300

    nova

--- setup.py
+++ setup.py
@@ -1,14 +1,32 @@
-import sys
 from cx_Freeze import setup, Executable
+import os
+import sys
 
-# Verifica se o sistema operacional é Windows
-base = None
+# Definir o caminho do ícone com base no sistema operacional
 if sys.platform == "win32":
-    base = "Win32GUI"  # Isso evita que a janela do console apareça
+    icone_path = os.path.join("assets", "icone.ico")  # Ícone para Windows
+else:
+    icone_path = os.path.join("assets", "icone.png")  # Ícone para Linux (se necessário)
 
+# Configuração do executável
+executables = [
+    Executable(
+        script="main.py",  # Arquivo principal do seu programa
+        icon=icone_path,   # Caminho para o ícone
+        base="Win32GUI" if sys.platform == "win32" else None,  # Usar Win32GUI para Windows
+        target_name="meu_ip"  # Nome do executável
+    )
+]
+
+# Configuração do setup
 setup(
-    name="MeuProjeto",
+    name="Meu IP",
     version="1.0",
-    description="Meu Projeto Simples",
-    executables=[Executable("main.py", base=base)],  # Adiciona o parâmetro base aqui
+    description="Um programa para mostrar o IP",
+    executables=executables,
+    options={
+        "build_exe": {
+            "include_files": [("assets", "assets")],  # Incluir a pasta assets no build
+        }
+    }
 )
