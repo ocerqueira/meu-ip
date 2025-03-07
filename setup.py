eb7f7bf5515358004978a9c227c59581b8dc3bff
Lucas Cerqueira <lukascerqueira@live.com>
2025-03-06 21:52:21 -0300

    new setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
+import sys
 from cx_Freeze import setup, Executable
+
+# Verifica se o sistema operacional é Windows
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"  # Isso evita que a janela do console apareça
 
 setup(
     name="MeuProjeto",
     version="1.0",
     description="Meu Projeto Simples",
-    executables=[Executable("main.py")],
+    executables=[Executable("main.py", base=base)],  # Adiciona o parâmetro base aqui
 )
